f4ef250da79e3b8af069619f814b52acdfb3ec74
Jasurbek Isokov <jasurisoqov685@gmail.com>
2022-03-28 11:46:43 +0300

    Tiem Interval

--- script.js
+++ script.js
@@ -5,32 +5,77 @@
 // BANKIST APP
 
 // Data
+
 const account1 = {
   owner: 'Jasurbek Isokov',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  movements: [200, 450, -400, 3000, -650, -130, 7000, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+  movementsDate: [
+    `2022-03-16T11:28:20.429Z`,
+    `2022-03-15T11:28:20.429Z`,
+    `2022-03-14T11:28:20.429Z`,
+    `2022-03-13T11:28:20.429Z`,
+    `2022-03-10T11:28:20.429Z`,
+    `2022-03-09T11:28:20.429Z`,
+    `2022-03-08T11:28:20.429Z`,
+    `2022-03-07T11:28:20.429Z`,
+    `2022-03-06T11:28:20.429Z`,
+  ],
 };
 
 const account2 = {
   owner: 'Joraqozi Turdialiyev',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  movements: [5000, 9400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+  movementsDate: [
+    `2022-03-16T11:28:20.429Z`,
+    `2022-03-15T11:28:20.429Z`,
+    `2022-03-14T11:28:20.429Z`,
+    `2022-03-12T11:28:20.429Z`,
+    `2022-03-11T11:28:20.429Z`,
+    `2022-03-10T11:28:20.429Z`,
+    `2022-03-10T11:28:20.429Z`,
+    `2022-02-15T11:28:20.429Z`,
+    `2022-01-14T11:28:20.429Z`,
+  ],
 };
 
 const account3 = {
   owner: 'Jonibek Munirov',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  movements: [2000, -200, 340, -300, -20, 5000, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+  movementsDate: [
+    `2022-03-16T11:28:20.429Z`,
+    `2022-03-15T11:28:20.429Z`,
+    `2022-03-14T11:28:20.429Z`,
+    `2022-03-12T11:28:20.429Z`,
+    `2022-03-11T11:28:20.429Z`,
+    `2022-03-10T11:28:20.429Z`,
+    `2022-03-10T11:28:20.429Z`,
+    `2022-02-15T11:28:20.429Z`,
+    `2022-01-14T11:28:20.429Z`,
+  ],
 };
 
 const account4 = {
   owner: 'Umid Rustamov',
-  movements: [430, 1000, 700, 50, 90],
+  movements: [430, 1000, 700, -550, 500, 9000, -100],
   interestRate: 1,
   pin: 4444,
+  movementsDate: [
+    `2022-03-16T11:28:20.429Z`,
+    `2022-03-15T11:28:20.429Z`,
+    `2022-03-14T11:28:20.429Z`,
+    `2022-03-12T11:28:20.429Z`,
+    `2022-03-11T11:28:20.429Z`,
+    `2022-03-10T11:28:20.429Z`,
+    `2022-03-10T11:28:20.429Z`,
+    `2022-02-15T11:28:20.429Z`,
+    `2022-01-14T11:28:20.429Z`,
+  ],
 };
 
 //localStorage.setItem('account1', account1);
@@ -72,6 +117,10 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const inputTransferAmount1 = document.querySelector('.form__input--amount');
+
+const balanceDate = document.querySelector('.balance__date');
+const date = document.querySelector('.date');
 // Functions
 
 // Time Out Opacity: 0;
@@ -92,13 +141,43 @@ setInterval(() => {
 
 const ekrangaTranzaksiyalarniChiqarish = function (obj) {
   containerMovements.innerHTML = '';
+  let nowDate = new Date().getDate();
+
+  let nowDateOy = new Date().getMonth();
+
+  let nowDateYil = new Date().getFullYear();
+  let bugunMil = new Date(nowDateYil, nowDateOy, nowDate);
+
+  console.log(`Bugun: ${bugunMil}`);
+  console.log(new Date(1647452112564));
   obj.movements.forEach(function (val, key) {
     let tekshir = val > 0 ? `deposit` : `withdrawal`;
 
+    let sanalar = new Date(
+      obj.movementsDate[obj.movementsDate.length - key - 1]
+    );
+    // console.log(sanalar);
+    let yil = sanalar.getFullYear();
+
+    let oy = sanalar.getMonth() + 1;
+
+    let kun = sanalar.getDate();
+
+    let vaqt;
+
+    if (nowDate == kun && nowDateYil == yil) {
+      vaqt = 'Bugun';
+    } else if (nowDate - kun == 1) {
+      vaqt = 'Kecha';
+    } else if (nowDate - kun <= 7) {
+      vaqt = 'Yaqin bir hafta ichida';
+    } else {
+      vaqt = `${kun}/${oy}/${yil}`;
+    }
     let qalay = `<div class="movements__row">
     <div class="movements__type movements__type--${tekshir}">
     ${key + 1} ${tekshir} </div>
-    <div class="movements__date">3 days ago</div>
+    <div class="movements__date">${vaqt} </div>
     <div class="movements__value">${val} € </div>
    </div>`;
     containerMovements.insertAdjacentHTML('afterbegin', qalay);
@@ -174,6 +253,48 @@ btnLogin.addEventListener('click', function (a) {
   //   labelWelcome.style.color = `red`;
   // }
 
+  [...document.querySelectorAll('.movements__row')].forEach(function (
+    val,
+    key
+  ) {
+    if (key % 2 === 0) {
+      val.style.backgroundColor = 'blue';
+    } else if (key % 2 === 1) {
+      val.style.backgroundColor = 'green';
+    }
+  });
+
+  // Date ni chiqarish
+
+  // let dateNow = new Date();
+
+  // let yil = dateNow.getFullYear();
+
+  // let oy = dateNow.getMonth() + 1;
+
+  // let kun = dateNow.getDate();
+
+  let hozirInternational = new Date();
+
+  let options = {
+    day: 'numeric',
+    month: 'numeric',
+    weekday: 'long',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+  };
+
+  let davlat = navigator.language;
+
+  console.log(davlat);
+
+  let uzb = Intl.DateTimeFormat(davlat, options).format(hozirInternational);
+
+  labelDate.textContent = uzb;
+
+  //  /Date ni chiqarish
+
   ekrangaTranzaksiyalarniChiqarish(kirganUser);
 
   labelBalance.textContent = `${sum(kirganUser)} €`;
@@ -223,6 +344,9 @@ btnTransfer.addEventListener('click', function (b) {
     transferTo.movements.push(transferAmount);
   }
 
+  if (transferAmount < 0 || sum(kirganUser) < transferAmount) {
+    inputTransferAmount1.textContent = '-----------';
+  }
   // console.log(kirganUser.movements);
 
   // console.log(transferTo);
@@ -316,54 +440,9 @@ btnSort.addEventListener('click', function (arr) {
 
 // /Sort and UnSort
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
-
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-/////////////////////////////////////////////////
-
-//  Time :
-
-// let date = new Date();
-// let mmb = date.getMinutes();
-// let ssb = date.getSeconds();
-
-// function currentTime() {
-//   let date = new Date();
-//   let mm = date.getMinutes();
-//   let ss = date.getSeconds();
-
-//   if (mm < 10) {
-//     mm = '0' + mm;
-//   }
-
-//   if (ss < 10) {
-//     ss = '0' + ss;
-//   }
-
-//   let time = `${mm}:${ss}`;
-
-//   document.getElementById('time').innerText = time;
-
-//   setTimeout(function () {
-//     currentTime();
-//     console.log(new Date().getTime() / 31536000);
-//   }, 1000);
-// }
-
-// currentTime();
-
-// /////////////////////////////////////////
-
 // Secundomer
+
+labelTimer.style.color = 'lightslategray';
 
 let min = document.getElementById('min');
 let sec = document.getElementById('sec');
@@ -386,3 +465,82 @@ setInterval(() => {
 }, 1000);
 
 // /Secundomer
+
+let index;
+
+let newArr = Array.from(
+  document.querySelectorAll('.movements__value'),
+  function (val, key) {
+    index = val.textContent;
+    for (let i = 0; i < index.length; i++) {
+      for (let j = 0; j < i; j++) {
+        if ((j = '€')) {
+        }
+      }
+    }
+    return index;
+  }
+);
+
+console.log(newArr);
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// LECTURES
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/////////////////////////////////////////////////
+
+//  Time :
+
+// let soat = document.getElementById('soat');
+// let minut = document.getElementById('minut');
+// let secund = document.getElementById('secund');
+
+function currentTime() {
+  let date = new Date();
+
+  let soat1 = date.getHours();
+  let mm = date.getMinutes();
+  let ss = date.getSeconds();
+
+  soat.innerHTML = soat1;
+  minut.innerHTML = mm;
+  secund.innerHTML = ss;
+
+  if (soat1 < 10) {
+    soat1 = '0' + soat1;
+  }
+
+  if (mm < 10) {
+    mm = '0' + mm;
+  }
+
+  if (ss < 10) {
+    ss = '0' + ss;
+  }
+
+  let time = `${soat1}:${mm}:${ss}`;
+
+  document.getElementById('time_date').innerText = time;
+
+  setTimeout(function () {
+    currentTime();
+    console.log(new Date().getTime() / 31536000);
+  }, 1000);
+}
+
+// currentTime();
+
+// /////////////////////////////////////////
+
+// Nol (0) dan boshlab 9999 gacha istalgan butun son
+// kiritilganda shu sonni string turida yozib chiqaruvchi dastur tuzing.
+// misol uchun : 0 kiritlsa nol degan , 100 kiritilsa yuz degan , 211 kiritilsa ikki yuz on bir degan yozuv chiqsin
